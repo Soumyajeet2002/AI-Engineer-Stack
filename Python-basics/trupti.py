ee31d330771166b7788dd3e93e9cf6bca90b3d6c
Truptimayee Dash <truptimayee4166@gmail.com>
2026-05-03 13:31:22 +0530

    Added python basics files

---
+++ Python-basics/trupti.py
@@ -0,0 +1,50 @@
+# 1 
+def func(x=[]):
+    x.append(1)
+    return x
+
+print(func())
+print(func())
+
+"""
+hii i am trupti
+"""
+
+
+# print twinkle twinkle little star
+print("Twinkle, twinkle, little star,");
+x = 5
+y = "John"
+print(x)
+print(y)
+x=10
+x="trupti"
+print(x)
+print(type(x))
+x,y,z=1,59,"rrr"
+x=y=z="2334"
+print(x+y)  
+
+r="awesome"
+def myfunc():
+    print("Python is " + r)
+
+myfunc()
+
+
+# t="thanks for the help"
+def myfunc():
+   global t     
+   t="thanks for the help"
+myfunc()
+print("Python is " + t)
+
+
+
+# DATA TYPES
+import random
+
+print(random.randrange(1,10))
+
+
+
